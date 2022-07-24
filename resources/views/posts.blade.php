786dd68869d4747812c3877152d79f7f4250ab8b
Garrett <garrettpyke@msn.com>
2022-07-23 19:42:11 -0600

    Add comment to posts view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,6 +8,7 @@
                 </h1>
 
                 <p>
+                    // TODO n+1 problem here - inside a loop accessing a relationship that hasn't yet been loaded
                     <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
                 </p>
 
