e07672a1df5f70865f062fd8320f89cc0ff0da7a
Garrett <garrettpyke@msn.com>
2022-07-24 20:28:39 -0600

    Finish db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use \App\Models\User;
 use \App\Models\Category;
+use \App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -25,25 +26,53 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        //* truncate will prevent duplication error for unique fields (Category name & slug)
+        //* truncate will prevent duplication error for unique fields when not seeding fresh (Category name & slug) 
         User::truncate();
         Category::truncate();
+        Post::truncate();
 
         $user = User::factory()->create();
 
-        Category::create([
+        $personal = Category::create([
             'name' => 'Personal',
             'slug' => 'personal'
         ]);
 
-        Category::create([
+        $hobbies = Category::create([
             'name' => 'Hobbies',
             'slug' => 'hobbies'
         ]);
 
-        Category::create([
+        $work = Category::create([
             'name' => 'Work',
             'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $hobbies->id,
+            'title' => 'My Backpacking Post',
+            'excerpt' => 'Excerpt for post',
+            'body' => 'Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet.',
+            'slug' => 'my-backpacking-post',                                                     'category_id' => 1
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Family Post',
+            'excerpt' => 'Excerpt for post',
+            'body' => 'Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet.',
+            'slug' => 'my-family-post',                                                     'category_id' => 1
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'excerpt' => 'Excerpt for post',
+            'body' => 'Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet. Lorem ipsum dolar sit amet.',
+            'slug' => 'my-work-post',                                                     'category_id' => 1
         ]);
     }
 }
