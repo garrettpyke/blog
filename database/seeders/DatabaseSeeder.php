4877bfd34ba8f95f727af58a45d124d2e1618694
Garrett <garrettpyke@msn.com>
2022-07-24 19:42:06 -0600

    Add db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use \App\Models\User;
+use \App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //* Create 10 users
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //* truncate will prevent duplication error for unique fields (Category name & slug)
+        User::truncate();
+        Category::truncate();
+
+        $user = User::factory()->create();
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Category::create([
+            'name' => 'Hobbies',
+            'slug' => 'hobbies'
+        ]);
+
+        Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
     }
 }
