d41ec6308e5ee0503e76f570ec661b5a4ab22698
Garrett <garrettpyke@msn.com>
2022-07-25 23:44:10 -0600

    Separate featured post in its own component & make dynamic, test OK'

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,9 +2,10 @@
     @include ('_posts-header')
     
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        <x-post-featured-card :post="$posts[0]" />
+        <!-- //*GTK: the : will pass thru the value of the variable instead of the string '$post' -->
 
-        <div class="lg:grid lg:grid-cols-2">
+        <!-- <div class="lg:grid lg:grid-cols-2">
             <x-post-card />
             <x-post-card />
         </div>
@@ -13,7 +14,7 @@
             <x-post-card />
             <x-post-card />
             <x-post-card />
-        </div>
+        </div> -->
     </main>
 
 </x-layout>
