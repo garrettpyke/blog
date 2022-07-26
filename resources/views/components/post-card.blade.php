bcfb68d466cc9ba924d7341899a06d713e700751
Garrett <garrettpyke@msn.com>
2022-07-26 13:49:10 -0600

    Apply some styling

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,4 +1,7 @@
-<article class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+@props(['post'])
+
+{{-- merge: Any attr passed (in this case from posts view) will be merged with the following --}}
+<article {{ $attributes->merge(['class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl']) }}>
     <div class="py-6 px-5">
         <div>
             {{-- TODO --}}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,15 +2,21 @@
     @include ('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts[0]" />
-        {{-- *GTK: the :colon will pass thru the value of the variable instead of the string '$post' --}}
+        @if ($posts->count()) {{-- if $posts collection is empty, error would be thrown w/o this --}}
+            <x-post-featured-card :post="$posts[0]" />
+            {{-- *GTK: the :colon will pass thru the value of the variable instead of the string '$post' --}}
 
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach ($posts->skip(1) as $post)
-            {{-- 'skip' is a collection method. Here skipping the first --}}
-                <x-post-card :post="$post" />
-            @endforeach
-        </div>
+            @if ($posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-2">
+                    @foreach ($posts->skip(1) as $post)
+                        {{-- 'skip' is a collection method. Here skipping the first --}}
+                        <x-post-card :post="$post" class="bg-red-500"/>
+                    @endforeach
+                </div>
+            @endif
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
     </main>
 
 </x-layout>
