f540a5cdd1d799a285e1a0ea8e03f687817a556b
Garrett <garrettpyke@msn.com>
2022-07-26 14:03:36 -0600

    Style changes to posts view: 1st row = featured post, 2nd row = 2 columns most recent, 3rd onward is 3 cols

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,10 +7,15 @@
             {{-- *GTK: the :colon will pass thru the value of the variable instead of the string '$post' --}}
 
             @if ($posts->count() > 1)
-                <div class="lg:grid lg:grid-cols-2">
+                <div class="lg:grid lg:grid-cols-6">
+                    {{-- 'skip' is a collection method. Here skipping the first --}}
                     @foreach ($posts->skip(1) as $post)
-                        {{-- 'skip' is a collection method. Here skipping the first --}}
-                        <x-post-card :post="$post" class="bg-red-500"/>
+                        {{-- !GTN: $loop, available in all loops. Handy debugging tool. --}}
+                        {{-- dd($loop) --}}
+                        <x-post-card 
+                            :post="$post"
+                            class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" 
+                        />
                     @endforeach
                 </div>
             @endif
