12c7db83eb5b7fa38b3034cd3199304dff9911d2
Garrett <garrettpyke@msn.com>
2022-07-26 11:20:04 -0600

    Update post-card component to display all remaining posts, test OK

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,16 @@
 <x-layout>
     @include ('_posts-header')
-    
+
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         <x-post-featured-card :post="$posts[0]" />
         {{-- *GTK: the :colon will pass thru the value of the variable instead of the string '$post' --}}
 
-        <!-- //*GTK: the : will pass thru the value of the variable instead of the string '$post' -->
-
-        <!-- <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
+        <div class="lg:grid lg:grid-cols-2">
+            @foreach ($posts->skip(1) as $post)
+            {{-- 'skip' is a collection method. Here skipping the first --}}
+                <x-post-card :post="$post" />
+            @endforeach
         </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div> -->
     </main>
 
 </x-layout>
