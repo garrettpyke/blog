a11d1ce8e3d6588bad0beaf65215a94c2b393f11
Garrett <garrettpyke@msn.com>
2022-07-26 10:29:05 -0600

    Pass posts data into posts view and featured-card component, test OK

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,6 +3,8 @@
     
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         <x-post-featured-card :post="$posts[0]" />
+        {{-- *GTK: the :colon will pass thru the value of the variable instead of the string '$post' --}}
+
         <!-- //*GTK: the : will pass thru the value of the variable instead of the string '$post' -->
 
         <!-- <div class="lg:grid lg:grid-cols-2">
