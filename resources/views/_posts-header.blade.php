9a3fe990770de903474224d58ca4669b4c4d99ec
Garrett <garrettpyke@msn.com>
2022-07-27 13:37:06 -0600

    Finish styling category drop-down

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -14,37 +14,35 @@
 
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+        <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
 
-            </div>
-                <button>Categories</button>
+            {{-- *GTN: this is Alpine.js at work. Script src in layout --}}
+            <div x-data="{ show: false }" @click.away="show = false">
+            {{-- We can execute any JS we want on a click event etc --}}    
+                <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">Categories
+                    {{-- This is just the arrow --}}
+                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
+                        height="22" viewBox="0 0 22 22">
+                        <g fill="none" fill-rule="evenodd">
+                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                            </path>
+                            <path fill="#222"
+                                d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                            </path>
+                        </g>
+                    </svg>
+                </button>
 
-                <div>
-                    <a href="#">One</a>
-                    <a href="#">Two</a>
-                    <a href="#">Three</a>
+                <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display: none">
+                    <a href="#"
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white focus:bg-blue-300">One</a>
+                    <a href="#"
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white focus:bg-blue-300">Two</a>
+                    <a href="#"
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white focus:bg-blue-300">A
+                        larger category</a>
                 </div>
             </div>
-
-            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>Category
-                </option>
-
-                @foreach ($categories as $category)
-                    <option value="{{ $category->slug }}">{{ $category->name }}</option>
-                @endforeach
-
-            </select>
-
-            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                height="22" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
-                    </path>
-                </g>
-            </svg>
         </div>
 
         <!-- Other Filters -->
@@ -71,7 +69,8 @@
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
+            <form method="GET" action="#"
+                class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300">
                 <input type="text" name="search" placeholder="Find something"
                     class="bg-transparent placeholder-black font-semibold text-sm">
             </form>
